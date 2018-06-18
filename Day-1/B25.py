66940abd18898fc4fb5d35a885b60bbbce55e602
VladPuzhanivskyi <36760792+VladPuzhanivskyi@users.noreply.github.com>
2018-06-18 16:55:21 +0300

    Create B25.py

---
+++ Day-1/B25.py
@@ -0,0 +1,20 @@
+import datetime 
+
+x = int(input())
+money = 20
+if x <= 30:
+    money += x * 9.86
+elif 30 < x <= 50:
+    money += 30 * 9.86 + (x - 10) * 11.22
+elif 50 < x <= 60:
+    money += 30 * 9.86 + 20 * 11.22 + (x - 50) * 13.06
+elif x > 60:
+    money += 30 * 9.86 + 20 * 11.22 + 10 * 13.06 + (x - 60) * 17.89
+
+print("Ти заплатиш %.2f грн" % money)
+
+def printTimeStamp(name):
+    print('Автор програми: ' + name)
+    print('Час компіляції: ' + str(datetime.datetime.now()))
+name = 'Puzhanivskyi Vladislav'
+printTimeStamp(name)
