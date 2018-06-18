cb1431c15045205dc7236d57e303ecf1b34df7c3
VladPuzhanivskyi <36760792+VladPuzhanivskyi@users.noreply.github.com>
2018-06-18 15:56:18 +0300

    Create Day1_11.py

---
+++ Day1_11.py
@@ -0,0 +1,15 @@
+import datetime
+
+a = list(input().split())
+a[0]=float(a[0])
+b = ["cm","foot","inch"]
+c = [0.01,0.3048,0.0254]
+d=c[b.index(a[1])]*a[0]
+f = c[b.index(a[3])]
+print("{:.2e}".format(d/f))
+
+def printTimeStamp(name):
+    print('Автор програми: ' + name)
+    print('Час компіляції: ' + str(datetime.datetime.now()))
+name = 'Puzhanivskyi Vladislav'
+printTimeStamp(name)
